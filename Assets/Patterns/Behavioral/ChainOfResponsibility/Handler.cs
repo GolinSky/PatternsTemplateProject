4e747c8af555299566a2f0ec334e77e6d3729d7b
Alex GolinSky <golinsky97@gmail.com>
2023-07-21 18:46:52 +0300

    - refactor  chain on responsobility

--- Assets/Patterns/Behavioral/ChainOfResponsibility/Handler.cs
+++ Assets/Patterns/Behavioral/ChainOfResponsibility/Handler.cs
@@ -2,17 +2,40 @@ namespace GofPatterns.Patterns.Behavioral.ChainOfResponsibility
 {
     public abstract class Handler:BaseHandler<IHandler>,IHandler
     {
+        protected abstract bool CanHandleRequest { get; }
+
         public virtual void Handle()
         {
-            NextHandler?.Handle();
+            if (CanHandleRequest)
+            {
+                NextHandler?.Handle();
+            }
+            else
+            {
+                OnHandle();
+            }
         }
+
+        protected abstract void OnHandle();
     }
     
     public abstract class Handler<THandleData>:BaseHandler<IHandler<THandleData>>, IHandler<THandleData>
     {
+
         public void Handle(THandleData data)
         {
-            NextHandler?.Handle(data);
+            if (CanHandleRequest(data))
+            {
+                NextHandler?.Handle(data);
+            }
+            else
+            {
+                OnHandle(data);
+            }
         }
+
+        protected abstract void OnHandle(THandleData handle);
+        protected abstract bool CanHandleRequest(THandleData data);
+
     }
 }
